b17da58000cb5d7fbb1296b8946895ec41834db5
klaflin <kellie.laflin.26@gmail.com>
2024-03-10 15:38:25 -0600

    toggle isFaceUp

--- MemoryGame/ContentView.swift
+++ MemoryGame/ContentView.swift
@@ -26,19 +26,21 @@ struct ContentView: View {
 
 
 struct CardView: View {
-    var isFaceUp: Bool = false;
+    @State var isFaceUp = false;
     
     var body: some View {
-        ZStack{
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("Card").font(.largeTitle)
             }
             else {
-                RoundedRectangle(cornerRadius: 12)
+                base
             }
+        }.onTapGesture{
+            isFaceUp.toggle()
         }
     }
 }

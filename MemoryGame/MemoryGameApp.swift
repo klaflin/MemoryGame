9c928534800608c59610eb0c052066ca578e30e3
klaflin <kellie.laflin.26@gmail.com>
2024-03-10 15:12:08 -0600

    creating memory game; init cardview

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by KELLIE LAFLIN on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
